fb704c4bd2095c2f96e3ca893a8645431caf6222
sansroman <sansroman@foxmail.com>
2017-12-25 17:39:48 +0800

    add cookie setting

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({ secret: 'dysniubi', cookie: { maxAge: 60*1000*60 }}))
+app.use(session({ secret: 'dysniubi', cookie: { maxAge: 60*1000*60 },resave:true,saveUninitialized:true}))
 app.use(express.static(path.join(__dirname, '../dist')));
 
 
